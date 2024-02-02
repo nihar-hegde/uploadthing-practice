29eb57bdfccbc13b93ee2e5cd78044e8489d6bd2
Nihar Hegde <niharhegde163@gmail.com>
2024-02-02 19:57:57 +0530

    form component setup

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,11 +1,13 @@
+import { Form } from "@/components/forms/form";
 import React from "react";
 
 const FormPage = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center ">
       <h1 className="flex items-center p-20 justify-center text-2xl font-semibold">
         Form Page
       </h1>
+      <Form />
     </div>
   );
 };
---
+++ src/components/forms/form.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <div>
+      <div>Fomr comksadlfuhg </div>
+    </div>
+  );
+};
