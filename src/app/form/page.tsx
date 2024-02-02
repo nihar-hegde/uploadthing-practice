0ab63362840053742544233a4e9b5afd204b2736
Nihar Hegde <niharhegde163@gmail.com>
2024-02-02 19:55:38 +0530

    Setup initial setup and shadcn setup added a form route

---
+++ src/app/form/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FormPage = () => {
+  return (
+    <div>
+      <h1 className="flex items-center p-20 justify-center text-2xl font-semibold">
+        Form Page
+      </h1>
+    </div>
+  );
+};
+
+export default FormPage;
