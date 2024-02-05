4c67fe36dd0f058e81d792ec45065f106aa300bc
Nihar Hegde <niharhegde163@gmail.com>
2024-02-05 12:20:34 +0530

    UPLADTHING setup uploadthing setup and basdic uplading done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,27 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
       <h1 className="text-3xl">Hello world</h1>
       <Link href={"/form"} className={buttonVariants({ variant: "default" })}>
-        Go TO Form
+        Go to Form
+      </Link>
+      <Link
+        href={"/uploadthing"}
+        className={buttonVariants({ variant: "default" })}
+      >
+        Go to Uploadthing
       </Link>
 
+      <Link
+        href={"/upload-button"}
+        className={buttonVariants({ variant: "default" })}
+      >
+        Go to UploadButton
+      </Link>
+      <Link
+        href={"/upload-dnd"}
+        className={buttonVariants({ variant: "default" })}
+      >
+        Go to upload dnd
+      </Link>
       <div>
         <AllData />
       </div>
---
+++ src/app/uploadthing/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UplaodPage = () => {
+  return (
+    <div className="flex items-center justify-center p-20">UplaodPage</div>
+  );
+};
+
+export default UplaodPage;
